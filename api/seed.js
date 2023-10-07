1140153d08749a1bf4c8b80c5d2e620dd706e293
Simran Singh <simran.develop@gmail.com>
2023-10-07 15:19:11 +1300

    feat(seed): initial bank data added in seed.js

---
+++ api/seed.js
@@ -0,0 +1,44 @@
+export const banksData = {
+  banks: [
+    {
+      name: "GlobalTrust Bank",
+      country: "USA",
+      established_year: 1950,
+      services: [
+        "Checking Account",
+        "Savings Account",
+        "Credit Card",
+        "Mortgage"
+      ]
+    },
+    {
+      name: "EuroSecure Bank",
+      country: "Germany",
+      established_year: 1920,
+      services: ["Checking Account", "Investment Banking", "Business Loans"]
+    },
+    {
+      name: "Pacific Financial",
+      country: "Australia",
+      established_year: 1975,
+      services: ["Checking Account", "Credit Card", "Home Loan", "Auto Loan"]
+    },
+    {
+      name: "AsiaSavings Bank",
+      country: "Japan",
+      established_year: 1960,
+      services: ["Checking Account", "Savings Account", "Investment Services"]
+    },
+    {
+      name: "Nordic Bank",
+      country: "Sweden",
+      established_year: 1880,
+      services: [
+        "Checking Account",
+        "Business Loans",
+        "Investment Banking",
+        "Insurance"
+      ]
+    }
+  ]
+}
